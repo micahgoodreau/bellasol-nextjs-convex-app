7d1125a799b087c7b5dc4aa91ccf6cab19e4e077
MICAH GOODREAU <micahgoodreau@gmail.com>
2026-01-19 17:34:59 -0500

    init for convex and nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+"use client"; 
+
+import { useQuery } from "convex/react";
 import Image from "next/image";
+import { api } from "../convex/_generated/api";
 
 export default function Home() {
+  const properties = useQuery(api.properties.get);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -16,8 +21,11 @@ export default function Home() {
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
           </h1>
+          <div className="mt-4">
+            {properties?.map(({ _id, unit_number, building_number }) => <div className="text-zinc-200" key={_id}>{unit_number} {building_number}</div>)}
+          </div>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            Test Looking for a starting point or more instructions? Head over to{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
