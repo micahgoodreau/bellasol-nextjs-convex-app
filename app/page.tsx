0229d0707704c3a3f726bc043cfb4a6823317a2f
Micah Goodreau <micahgoodreau@gmail.com>
2026-01-20 18:25:50 -0500

    fix leepa query

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { api } from "../convex/_generated/api";
 
 export default function Home() {
-  const leepa = useQuery(api.leepa.get);
+  const leepa = useQuery(api.leepa.getAll);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
