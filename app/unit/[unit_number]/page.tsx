1215795f4bd5c41af34b67d330545f195ecfe1b3
Micah Goodreau <micahgoodreau@gmail.com>
2026-02-20 11:24:16 -0500

    Disable leepa_sales query in page.tsx
    Comment out leepa_sales query for unit number.

--- app/unit/[unit_number]/page.tsx
+++ app/unit/[unit_number]/page.tsx
@@ -12,7 +12,7 @@ export default function Page({
 }) {
   const { unit_number } = use(params);
   const leepa_owner = useQuery(api.leepa.getUnitByUnitNumber, { unit_number: parseFloat(unit_number.toString()) });
-  //const leepa_sales = useQuery(api.leepa.getSalesByUnitNumber, { unit_number: parseFloat(unit_number.toString()) });
+  const leepa_sales = null; // useQuery(api.leepa.getSalesByUnitNumber, { unit_number: parseFloat(unit_number.toString()) });
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
